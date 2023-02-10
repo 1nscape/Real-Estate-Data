e7e959855b3e7f214fae1beab424720f078cad9d
Max <max.belenchuk@gmail.com>
2023-02-09 20:58:50 -0400

    passport.js file further edited. Created a function to relate to email and user name and done function, once user inputs information.

--- config/passport.js
+++ config/passport.js
@@ -3,3 +3,19 @@ var LocalStrategy = require("password-local").Strategy;
 
 var db = require("../models");
 
+passport.use(new LocalStrategy(
+
+    {
+        usernameField : "email"
+    },
+
+    function(email, password, done) {
+        
+        db.User.findOne({
+            where: {
+                email: email 
+            }
+        }).then(function(dbUser))
+    }
+
+))
