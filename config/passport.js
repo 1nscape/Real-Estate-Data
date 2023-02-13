fa1852a26935f32f07b892c3322746a88be0848e
Max <max.belenchuk@gmail.com>
2023-02-13 19:39:22 -0400

    Passport-local authenticator complete according to documentation. Next step is middleware. Conditional statements created in case user inputs incorrect data.

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,44 @@
-var passport = require("passport"); 
-var LocalStrategy = require("password-local").Strategy; 
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
+
 passport.use(new LocalStrategy(
-
     {
-        usernameField : "email"
+        usernameField: "email"
     },
+    function (email, password, done) {
 
-    function(email, password, done) {
-        
         db.User.findOne({
             where: {
-                email: email 
+                email: email
             }
-        }).then(function(dbUser))
-    }
+        }).then(function (dbUser) {
+            if (!dbUser) {
+                return done(null, false, {
+                    message: "Incorrect email."
+                });
+            }
 
-))
+            else if (!dbUser.validPassword(password)) {
+                return done(null, false, {
+                    message: "Incorrect password."
+                });
+            }
+
+            return done(null, dbUser);
+        });
+    }
+)
+);
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+module.exports = passport;
