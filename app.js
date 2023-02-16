b7686eb7a2fea1aad8d5eac905d7f57494398b48
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-02-15 21:58:59 -0400

    Writing data to API

--- app.js
+++ app.js
@@ -35,26 +35,54 @@ getData();
 });
 
 async function getData() {
-    console.log(url);
-      let response = await fetch(url, options)
-          let data = await response.json();
-          let propertyData = data.home_search.results;
-          for (var i in propertyData){
-            bathrooms = propertyData[i].description.baths;
-            bedrooms = propertyData[i].description.beds; 
-            type = propertyData[i].description.type;
-            listPrice = propertyData[i].list_price;
-            primaryPhoto = propertyData[i].primary_photo.href;
-            //address = propertyData[i].location.address.coordinate.line;
+  console.log(url);
+    let response = await fetch(url, options)
+        let data = await response.json();
+        let propertyData = data.home_search.results;
+        for (var i in propertyData){
+          bathrooms = propertyData[i].description.baths;
+          bedrooms = propertyData[i].description.beds; 
+          type = propertyData[i].description.type;
+          listPrice = propertyData[i].list_price;
+          primaryPhoto = propertyData[i].primary_photo.href;
+          //address = propertyData[i].location.address.coordinate.line;
+        
+          createData(bedrooms, bathrooms, type, listPrice, primaryPhoto);
+
+        console.log("bathrooms: " + bathrooms + " /n Bedrooms" + bedrooms + " Photo: " + primaryPhoto);
+
+        }      
+        
+        
+}
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000")
+})
+
+const {createPool} = require('mysql')
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password : "root",
+  database: "realEstate_db",
+})
+
+function createData(bedrooms, bathrooms, type, listPrice, primaryPhoto){
+  // let sql = 'INSERT INTO houses (number_of_rooms, number_of_bathrooms, property_type, list_price, primary_photo) values ?'
+  let sql = 'INSERT INTO houses SET ?'
+
+  const values = {number_of_rooms: bedrooms, number_of_bathrooms: bathrooms, property_type: type, list_price: listPrice, primary_photo: primaryPhoto}  
+
+  pool.query(sql, values, function(err, result){
+      if(bedrooms && bathrooms) {
+      if (err) throw err;
   
-          console.log("bathrooms: " + bathrooms + " /n Bedrooms" + bedrooms + " Photo: " + primaryPhoto);
+      console.log("Data Uploaded");
   
-          }          
-          
-  }
-  
-  //getData();
-  
-  app.listen(3002, function(){
-    console.log("Server is running on port 3002")
-  })
+    }
+  });
+}
+
